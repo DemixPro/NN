7b77b855ea0a90b0e4b3501aa7b56ebbe06c5377
DemixPro <110328394+DemixPro@users.noreply.github.com>
2024-10-22 16:55:16 +0300

    Update war-tycoon-source.lua

--- war-tycoon-source.lua
+++ war-tycoon-source.lua
@@ -25,7 +25,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "War Tycoon - 1.3",
+    Title = "War Tycoon - 1.3.1",
     SubTitle = "ода шедевротайкон - nnhub",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
