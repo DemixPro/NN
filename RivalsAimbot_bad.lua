3b30a3fc9e585a5e5a1eb28e4fadef8917f972c0
DemixPro <110328394+DemixPro@users.noreply.github.com>
2024-11-01 21:03:17 +0300

    Update RivalsAimbot_bad.lua

--- RivalsAimbot_bad.lua
+++ RivalsAimbot_bad.lua
@@ -159,4 +159,4 @@ Tabs.Aimbot:AddInput("Input", {
 
 Window:SelectTab(1)
 
-queue_on_teleport("https://raw.githubusercontent.com/DemixPro/NN/refs/heads/main/RivalsAimbot_bad.lua")
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/DemixPro/NN/refs/heads/main/RivalsAimbot_bad.lua"))()')
